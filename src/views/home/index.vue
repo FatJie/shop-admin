cbb328f1ec9f9e1631514c98723a63b0710f5843
fatjie <372761781@qq.com>
2022-08-07 15:08:54 +0800

    chore: add vuex4

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,13 @@
 <template>
   <h1>首页</h1>
+  <div>{{ store.state.count }}</div>
+  <div>{{ $store.state.count }}</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useStore } from '../../store'
+
+const store = useStore()
+
+console.log(store.state.count)
+</script>
